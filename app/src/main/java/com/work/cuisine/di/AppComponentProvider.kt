3362504adf564b4ccee3123d2197edfd63693b6e
Natalia <natashadihtiar@gmail.com>
2020-04-11 14:41:21 +0300

    Теперь можно создавать и получать компонент даггера через статический метод

---
+++ app/src/main/java/com/work/cuisine/di/AppComponentProvider.kt
@@ -0,0 +1,14 @@
+package com.work.cuisine.di
+
+import com.work.cuisine.Application
+
+object AppComponentProvider {
+    lateinit var appComponent: AppComponent
+    fun provideAppComponent(context: Application) =
+        if (this::appComponent.isInitialized) appComponent
+        else {
+            appComponent = DaggerAppComponent.factory().create(context)
+            appComponent
+
+        }
+}
